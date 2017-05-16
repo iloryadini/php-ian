8ff66948caea42086b8bd5304a69921500a0613a
ianloryadini <loryadini@gmail.com>
2017-05-16 15:19:27 +0700

    lengkapin contact

--- tests/contact.py
+++ tests/contact.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 import time
 import unittest
 import logging
-
+import sys
 #create a logger
 # logger = logging.getLogger(__name__)
 # logger.setLevel(logging.INFO)
@@ -26,7 +26,10 @@ import logging
 # #add the handlers
 # logger.addHandler(handler_info)
 # logger.addHandler(handler_critical)
-client_urls = ['http://qaflowerstudio.qeon.co.id/contact-us' , 'http://qaqdh.qeoninteractive.com/#contact']
+client_urls = ['http://qaqdh.qeoninteractive.com/#contact' , 'http://qaflowerstudio.qeon.co.id/contact-us']
+
+# phones = ['phone' , 'telp']
+# pesan = ['comment' , 'message']
 # element_client = ['name' , 'email' , 'phone' , 'comment' ,'company' ,'telp']
 class ContactUs(unittest.TestCase):
 	def setUp(self):
@@ -47,7 +50,27 @@ class ContactUs(unittest.TestCase):
 	def test_ContactUs(self):
 		driver = self.driver
 		driver.find_element_by_name('name').send_keys('qaqeon')
+		time.sleep(1)
 		driver.find_element_by_name('email').send_keys('qadev@qeon.co.id')
+		time.sleep(1)
+		if "No.Telp" in driver.page_source:
+			driver.find_element_by_name('telp').send_keys('081234567')
+		else:
+			driver.find_element_by_name('phone').send_keys('081234567')
+			return
+		time.sleep(1)
+		if "qdh" in driver.page_source:
+			driver.find_element_by_name('message').send_keys('Sorry for spamming once a week, this is automatic test. Please Dont Reply This email')
+		else:
+			driver.find_element_by_name('comment').send_keys('Sorry for spamming once a week, this is automatic test. Please Dont Reply This email')
+			return
+		time.sleep(1)
+		if "Company" in driver.page_source:
+			driver.find_element_by_name('company').send_keys('PT QA QEON INTERACTIVE')
+		else:
+			return
+		
+
 
 	def tearDown(self):
 		self.driver.quit()
